e99d24f7bec48bae0a72d5e3c2b3ca5ced8d082d
fuzzy <17826806525@163.com>
2019-05-06 17:19:48 +0800

    p151

--- src/main.js
+++ src/main.js
@@ -5,10 +5,19 @@ import VueRouter from 'vue-router'
 // 1.2 安装路由
 Vue.use(VueRouter)
 
+// 导入格式化时间的插件
+import moment from 'moment'
+// 定义全局的过滤器
+Vue.filter('dateFormat', function(dataStr, pattern = "YYYY-MM-DD HH:mm:ss"){
+    return moment(dataStr).format(pattern)
+})
+
 // 2.1 导入 vue-resource
 import VueResource from 'vue-resource'
 // 2.2 安装 vue-resource
 Vue.use(VueResource)
+// 设置请求的根路径
+Vue.http.options.root = 'http://www.liulongbin.top:3005'
 
 
 // 导入 MUI 的样式
