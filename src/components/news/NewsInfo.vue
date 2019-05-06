6424a8277aed532eca970c36d7d8abccebd0ac4f
fuzzy <17826806525@163.com>
2019-05-06 22:50:17 +0800

    p152

---
+++ src/components/news/NewsInfo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h3>新闻详情 -- {{ id }}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            id:this.$route.params.id // 将 URL 地址中传递过来的 Id值，挂载到 data 上，方便以后调用
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
