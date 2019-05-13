b568e023c16c832da3261e1ffd1840fc16c01c4c
fuzzy <17826806525@163.com>
2019-05-13 17:14:55 +0800

    p180 3:54

--- src/components/goods/GoodsInfo.vue
+++ src/components/goods/GoodsInfo.vue
@@ -1,60 +1,108 @@
 <template>
   <div class="goodsinfo-container">
+
+    <div class="ball"></div>
+
     <!-- 商品轮播图区域 -->
     <div class="mui-card">
       <div class="mui-card-content">
         <div class="mui-card-content-inner">
-            <swiper :lunbotuList="lunbotu"></swiper>
+          <swiper :lunbotuList="lunbotu" :isfull="false"></swiper>
         </div>
       </div>
     </div>
 
     <!-- 商品购买区域 -->
     <div class="mui-card">
-      <div class="mui-card-header">页眉</div>
+      <div class="mui-card-header">{{ goodsinfo.title }}</div>
       <div class="mui-card-content">
-        <div class="mui-card-content-inner">包含页眉页脚的卡片，页眉常用来显示面板标题，页脚用来显示额外信息或支持的操作（比如点赞、评论等）</div>
+        <div class="mui-card-content-inner">
+          <p class="price">
+            市场价:
+            <del>￥{{ goodsinfo.market_price }}</del>&nbsp;&nbsp;销售价：
+            <span class="now_price">￥{{ goodsinfo.sell_price }}</span>
+          </p>
+          <p>
+            购买数量：
+            <numbox></numbox>
+          </p>
+          <p>
+            <mt-button type="primary" size="small">立即购买</mt-button>
+            <mt-button type="danger" size="small">加入购物车</mt-button>
+          </p>
+        </div>
       </div>
-      <div class="mui-card-footer">页脚</div>
     </div>
 
     <!-- 商品参数区域 -->
     <div class="mui-card">
-      <div class="mui-card-header">页眉</div>
+      <div class="mui-card-header">商品参数</div>
       <div class="mui-card-content">
-        <div class="mui-card-content-inner">包含页眉页脚的卡片，页眉常用来显示面板标题，页脚用来显示额外信息或支持的操作（比如点赞、评论等）</div>
+        <div class="mui-card-content-inner">
+          <p>商品货号：{{ goodsinfo.goods_no }}</p>
+          <p>库存情况：{{ goodsinfo.stock_quantity }}件</p>
+          <p>上架时间：{{ goodsinfo.add_time | dateFormat }}</p>
+        </div>
       </div>
-      <div class="mui-card-footer">页脚</div>
+      <div class="mui-card-footer">
+        <mt-button type="primary" size="large" plain @click="goDesc(id)">图文介绍</mt-button>
+        <mt-button type="danger" size="large" plain @click="goComment(id)">商品详情</mt-button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 // 导入轮播图组件
-import swiper from '../subcomponents/swiper.vue'
-
+import swiper from "../subcomponents/swiper.vue";
+// 导入 数字选择框 组件
+import numbox from "../subcomponents/goodsinfo_numberbox.vue";
 
 export default {
   data() {
     return {
       id: this.$route.params.id, // 将路由参数对象中的 id 挂载到 data，方便后期调用
-      lunbotu: [] // 轮播图的数据
+      lunbotu: [], // 轮播图的数据
+      goodsinfo: {}, // 获取到的商品信息
+      ballFlag: false // 控制小球的隐藏和显示的标识符
     };
   },
   created() {
     this.getLunbotu();
+    this.getGoodsInfo();
   },
   methods: {
     getLunbotu() {
       this.$http.get("api/getthumimages/" + this.id).then(result => {
         if (result.body.status === 0) {
+          // 先循环轮播图数组的每一项，为 item 添加 img 属性，因为 轮播图 组件中，只认识 item.img,不认识 item.src
+          result.body.message.forEach(item => {
+            item.img = item.src;
+          });
           this.lunbotu = result.body.message;
         }
       });
+    },
+    getGoodsInfo() {
+      // 获取商品的信息
+      this.$http.get("api/goods/getinfo/" + this.id).then(result => {
+        if (result.body.status === 0) {
+          this.goodsinfo = result.body.message[0];
+        }
+      });
+    },
+    goDesc(id){
+      // 点击使用编程式导航跳转到 图文介绍页面
+      this.$router.push({name:'goodsdesc', params:{id}})
+    },
+    goComment(id){
+      // 点击跳转到评论页面
+      this.$router.push({name:'goodscomment', params:{id}})
     }
   },
-  components:{
-      swiper
+  components: {
+    swiper,
+    numbox
   }
 };
 </script>
@@ -63,5 +111,26 @@ export default {
 .goodsinfo-container {
   background-color: #eee;
   overflow: hidden;
+  .now_price {
+    color: red;
+    font-size: 16px;
+    font-weight: bold;
+  }
+  .mui-card-footer {
+    display: block;
+    button {
+      margin: 15px 0;
+    }
+  }
+  .ball{
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    background-color: red;
+    position: absolute;
+    z-index: 2;
+    top: 390px;
+    left: 150px;
+  }
 }
 </style>
